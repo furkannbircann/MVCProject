25db3e1cb0e8cf7d3c622fa93c66a661715c085c
Furkan Bircan <furkhanbircan@gmail.com>
2022-11-08 18:13:31 +0300

    ActionResults & Redirectoaction method ...

---
+++ MVCProject.Web/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCProject.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            return RedirectToAction("Index","Test");
+        }
+        public IActionResult ContentResult()
+        {
+            return Content("Content Result is here");
+        }
+        public IActionResult JsonResult()
+        {
+            return Json(new { Id = 1, Name = "Book", UnitPrice = 18 });
+        }
+        public IActionResult EmptyResult()
+        {
+            return new EmptyResult();
+        }
+    }
+}
