4e3068fc0030a68312510c618cc06a6c285ceebf
Furkan Bircan <furkhanbircan@gmail.com>
2022-11-21 00:12:21 +0300

    Created CRUD operation for Product Entity

---
+++ MVCProject.Web/Models/DataAccess/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using MVCProject.Web.Models.Entities.Concrete;
+
+namespace MVCProject.Web.Models.DataAccess
+{
+    public class ProductRepository
+    {
+        private static List<Product> _products;
+        public List<Product> GetAllProducts() => _products;
+        public void ProductAdd(Product product) => _products.Add(product);
+
+        public void ProductDelete(Product product)
+        {
+            var hasProduct = _products.FirstOrDefault(p => p.Id == product.Id);
+            if (hasProduct == null)
+            {
+                throw new Exception("Product not found");
+            }
+            _products.Remove(hasProduct);
+        }
+
+        public void ProductUpdate(Product product)
+        {
+            var hasProduct = _products.FirstOrDefault(p => p.Id == product.Id);
+            if (hasProduct == null)
+            {
+                throw new Exception("Product not found");
+            }
+            hasProduct.Name = product.Name;
+            hasProduct.Price = product.Price;
+            hasProduct.Stock = product.Stock;
+
+            var index = _products.FindIndex(p => p.Id == product.Id);
+            _products[index] = hasProduct;
+        }
+    }
+}
+
